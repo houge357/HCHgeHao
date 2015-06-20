209750f71e5b3619b46a8feabc4b256fe27a7a9a
xiyouji <hongloumeng5555@163.com>
2015-06-20 21:25:48 +0800

    初始化项目

---
+++ hello/hello/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hello
+//
+//  Created by wxhl on 15-6-19.
+//  Copyright (c) 2015年 wxhl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ hello/hello/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  hello
+//
+//  Created by wxhl on 15-6-19.
+//  Copyright (c) 2015年 wxhl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
+    
+    UIView
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
